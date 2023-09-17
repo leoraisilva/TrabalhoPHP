0977c940ad83ee7fe895ddf9cd685ee99818d892
lsilva <142047276+leoraisilva@users.noreply.github.com>
2023-09-17 19:34:39 -0300

    Delete arquivo directory

--- arquivo/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $arquivo = "arquivo.txt";
-    $FILES = fopen($file, 'a');
-    $conteudo = "Construindo um arquivo text";
-    fwrite($FILES, $conteudo);
-    fclose($FILES);
-    echo "arquivo criado com sucesso";
